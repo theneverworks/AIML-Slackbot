06eaf423e706f1dd5aac3caf59203084e028ff00
theneverworks <thelegend@theneverworks.com>
2017-01-29 19:03:42 -0700

    added web message handler

---
+++ services/webmessagehandler.py
@@ -0,0 +1,45 @@
+##########################################################################################
+# Sam - Home and Office Automation Web Message Handler
+# (Sam Web Message Handler)
+#
+# Version 1.0
+#
+# Receives messages in a simple URL from other processes
+# 
+##########################################################################################
+
+from flask import Flask, request, redirect
+import pika
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET', 'POST'])
+def tell_jarvis():
+    saywhat = request.values.get('Message', None)
+    message = request.args.get('s', None)
+    print saywhat
+
+    credentials = pika.PlainCredentials('user', 'password')
+    parameters = pika.ConnectionParameters('IP_ADDRESS',
+                                       PORT,
+                                       'queue',
+                                       credentials)
+
+    connection = pika.BlockingConnection(parameters)
+
+    channel = connection.channel()
+
+    result = channel.queue_declare(exclusive=True)
+    queue_name = result.method.queue
+
+    channel.queue_bind(exchange='messages', queue=queue_name)
+
+    channel.basic_publish(exchange='messages',
+                          routing_key='',
+                          body=message)
+    print(" [x] Sent '" + message + "'")
+    connection.close()
+    return "Sent to Sam:" + message
+
+if __name__ == "__main__":
+    app.run(host='IP_ADDRESS',port=PORT,debug=True)
